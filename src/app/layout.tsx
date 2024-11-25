1ccc7d0ecf895841c590a4c8c5cd38771a319a6d
Aaron <aaalenortbless@gmail.com>
2024-11-25 17:03:38 -0500

    pruebas2d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,16 +6,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
